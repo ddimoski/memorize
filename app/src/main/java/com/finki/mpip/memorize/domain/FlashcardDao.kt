15a1c0d3d7e5942cdeb21445c5ce239a17951598
davor.dimoski <davor.dimoski@hotmail.com>
2020-09-09 03:52:20 +0200

    add entity and dao

---
+++ app/src/main/java/com/finki/mpip/memorize/domain/FlashcardDao.kt
@@ -0,0 +1,25 @@
+package com.finki.mpip.memorize.domain
+
+import androidx.room.*
+
+@Dao
+interface FlashcardDao {
+    @Query("SELECT * FROM flashcard WHERE category LIKE :category")
+    fun getAllByCategory(category: String): List<Flashcard>
+
+    @Query("SELECT * FROM flashcard")
+    fun getAll(): List<Flashcard>
+
+    @Query("SELECT * FROM flashcard WHERE id = :id")
+    fun getById(id: Long): Flashcard
+
+    @Insert
+    fun addFlashcard(newFlashcard: Flashcard): Long
+
+    @Update
+    fun updateFlashcard(flashcard: Flashcard): Int
+
+    @Delete
+    fun deleteFlashcard(flashcard: Flashcard): Int
+    
+}
