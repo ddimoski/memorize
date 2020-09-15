badfce5beb7785a9243017bf2313bd927b3d3aab
davor.dimoski <davor.dimoski@hotmail.com>
2020-09-15 15:48:10 +0200

    finishing up last touches

--- app/src/main/java/com/finki/mpip/memorize/HomeFragment.kt
+++ app/src/main/java/com/finki/mpip/memorize/HomeFragment.kt
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.finki.mpip.memorize.database.AppDatabase
 import com.finki.mpip.memorize.model.User
 import com.finki.mpip.memorize.view_model.FlashcardViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,11 +29,10 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var flashcardViewModel: FlashcardViewModel
-    private lateinit var homeActivity: HomeActivity
     private lateinit var loggedInUser: User
-    private lateinit var db: AppDatabase
+    private lateinit var welcomeText: TextView
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,26 +40,17 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
-        homeActivity = HomeActivity()
         sharedPreferences = activity?.getSharedPreferences("com.finki.mpip.memorize", Context.MODE_PRIVATE)!!
         val userId = sharedPreferences?.getString("userId", "empty")
-        /*if (userId != "empty") {
-            doAsync{
-                loggedInUser = db.userDao().getById(userId!!)
-                Log.w("Success: ", "Async task done")}
-        }*/
-        //db = AppDatabase.getInstance(activity!!.applicationContext)
-        //loggedInUser = homeActivity.loggedInUser
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        welcomeText = view.findViewById(R.id.welcome_logged_in)
+        return view
     }
 
     companion object {
@@ -85,6 +79,12 @@ class HomeFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        loggedInUser = (activity as HomeActivity).loggedInUser
+        welcomeText.text = String.format("Welcome %s", loggedInUser.displayName)
     }
 
 }
